524fc6fa51d9e00377a288422bbaad41e86e02a5
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2025-12-15 18:35:19 -0300

    Adicionado a Rota Livros

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 //Importar o Express
 const express = require("express"); //Função que cria um servidor Express
+const rotaLivro = require("./rotas/livro")
 const app = express(); // Criou uma aplicação Express
 const port = 8000; //Porta do site
 
-//Criação de uma rota
-// Em resumo, a função do 'get' do express recebe uma  request ('req') e response ('res'). Request: É o que nos é mandado (pessoa que chama o nosso serviço manda uma request) e o que é devolvido para ela é a Response
-app.get('/', (req, res) => {
-  res.send("Olá, mundo!");
-}); // A '/' é o path e o 'req, res': requisição e response
+app.use('/livros', rotaLivro)
  
 //app.listen(...): Escutando as mensagens que ela recebe nessa porta '8000'
 app.listen(port, () => {
---
+++ rotas/livro.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const router = Router()
+
+//Criação de uma rota
+// Em resumo, a função do 'get' do express recebe uma  request ('req') e response ('res'). Request: É o que nos é mandado (pessoa que chama o nosso serviço manda uma request) e o que é devolvido para ela é a Response
+router.get('/', (req, res) => {
+  res.send("Olá, mundo!");
+}); // A '/' é o path e o 'req, res': requisição e response
+
+module.exports = router //Será exportado o router para caso queira utilizar em outros arquivos (tipo export default no react)
