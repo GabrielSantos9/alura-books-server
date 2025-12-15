9bf16e3ee81ef0c5a63c7be94adf9741449bbcf3
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2025-12-15 19:54:55 -0300

    Try e catch

--- rotas/livro.js
+++ rotas/livro.js
@@ -4,7 +4,12 @@ const router = Router()
 //Criação de uma rota
 // Em resumo, a função do 'get' do express recebe uma  request ('req') e response ('res'). Request: É o que nos é mandado (pessoa que chama o nosso serviço manda uma request) e o que é devolvido para ela é a Response
 router.get('/', (req, res) => {
-  res.send("Olá, mundo!");
+  try {
+    res.send("Olá, mundo!");
+  } catch (error) {
+    res.status(500)
+    res.send(error.message)
+  }
 }); // A '/' é o path e o 'req, res': requisição e response
 
 router.post('/', (req, res) => {
