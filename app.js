633826342abb51a6b28da679da034884ed8a4519
GabrielSantos9 <gabrielsantos9.programador@gmail.com>
2025-12-27 14:03:23 -0300

    Sincronizado a API com o Front-end
    Instalado o cors usando npm e sincronizado a api com o front end

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 const express = require("express"); //Função que cria um servidor Express
 const rotaLivro = require("./rotas/livro")
 const app = express(); // Criou uma aplicação Express
-const port = 8000; //Porta do site
+const port = 8000; // Porta do site
+const cors = require("cors")
 
 app.use(express.json()) //Significa que a nossa aplicação aceita receber body do tipo JSON, ela consegue ler. Com ela agora é possível adicionar um livro no json
+app.use(cors({origin: "*"})) //Utilizado para que permita que seja feito requisições e responses. Obs: esse código tem que ficar antes do 'app.use('/livros', rotaLivro)', se não da erro
 app.use('/livros', rotaLivro)
  
 //app.listen(...): Escutando as mensagens que ela recebe nessa porta '8000'
